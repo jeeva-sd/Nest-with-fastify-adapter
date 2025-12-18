2e99cbd13e2e0e4d380b2e7a526cc69f0ddc3e56
jeeva-sd <jeevasubash64@gmail.com>
2025-12-19 00:48:27 +0530

    refactor(eco-apps): remove unused PermissionCacheService from providers

--- src/modules/eco-apps/eco-apps.module.ts
+++ src/modules/eco-apps/eco-apps.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PermissionCacheService } from '../roles/permission-cache.service';
 import { RoleModule } from '../roles/role.module';
 import { EcoAppsController } from './eco-apps.controller';
 import { EcoAppsService } from './eco-apps.service';
@@ -7,7 +6,7 @@ import { EcoAppsService } from './eco-apps.service';
 @Module({
     imports: [RoleModule],
     controllers: [EcoAppsController],
-    providers: [EcoAppsService, PermissionCacheService],
+    providers: [EcoAppsService],
     exports: [EcoAppsService]
 })
 export class EcoAppsModule {}
